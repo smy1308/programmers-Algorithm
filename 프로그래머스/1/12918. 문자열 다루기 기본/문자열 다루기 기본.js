51b562940cff79346c8503611d6956d38761327e
Min Young <146905861+smy1308@users.noreply.github.com>
2023-12-07 10:59:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = parseInt(s)
+    if ((s.length === 4 || s.length ===6) && s == answer) {
+        answer = true
+    } else {
+        answer = false
+    }
+    return answer
+}
